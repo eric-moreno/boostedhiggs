31d15d7dc165c3eb081793d79a3770973b7abc21
Dylan Rankin <dylan.sheldon.rankin@cern.ch>
2021-10-26 10:36:13 -0500

    updating fileset/xsec

--- data/make_xsec.py
+++ data/make_xsec.py
@@ -1,7 +1,5 @@
 import json
 
-#'DYJetsToLL_Pt-100To250_TuneCP5_13TeV-amcatnloFXFX-pythia8', 'DYJetsToLL_Pt-250To400_TuneCP5_13TeV-amcatnloFXFX-pythia8', 'DYJetsToLL_Pt-400To650_TuneCP5_13TeV-amcatnloFXFX-pythia8', 'DYJetsToLL_Pt-650ToInf_TuneCP5_13TeV-amcatnloFXFX-pythia8', 'GluGluHTauTau_13TeV', 'GluGluHToTauTau_M125_13TeV_powheg_pythia8', 'MET_pancakes-02_Run2018A-12Nov2019_UL2018-v3', 'MET_pancakes-02_Run2018B-12Nov2019_UL2018_rsb-v1', 'MET_pancakes-02_Run2018C-12Nov2019_UL2018_rsb-v1', 'MET_pancakes-02_Run2018D-12Nov2019_UL2018_rsb-v2', 'QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8', 'QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8', 'QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8', 'QCD_HT500to700_TuneCP5_13TeV-madgraphMLM-pythia8', 'QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8', 'ST_s-channel_4f_hadronicDecays_TuneCP5_13TeV-madgraph-pythia8', 'ST_s-channel_4f_leptonDecays_TuneCP5_13TeV-madgraph-pythia8', 'ST_t-channel_antitop_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8', 'ST_t-channel_top_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8', 'ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8', 'ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8', 'SingleMuon_pancakes-02_Run2018A-12Nov2019_UL2018_rsb-v1', 'SingleMuon_pancakes-02_Run2018B-12Nov2019_UL2018-v2', 'SingleMuon_pancakes-02_Run2018C-12Nov2019_UL2018-v2', 'SingleMuon_pancakes-02_Run2018D-12Nov2019_UL2018-v4', 'TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8', 'TTToHadronic_TuneCP5_13TeV-powheg-pythia8', 'TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8', 'VBFHToTauTau_M125_13TeV_powheg_pythia8', 'WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToQQ_HT-800toInf_qc19_3j_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToQQ_HT400to600_qc19_3j_TuneCP5_13TeV-madgraphMLM-pythia8', 'WJetsToQQ_HT600to800_qc19_3j_TuneCP5_13TeV-madgraphMLM-pythia8', 'WminusHToTauTau_M125_13TeV_powheg_pythia8', 'WplusHToTauTau_M125_13TeV_powheg_pythia8', 'ZHToTauTau_M125_13TeV_powheg_pythia8', 'ZJetsToQQ_HT-800toInf_qc19_4j_TuneCP5_13TeV-madgraphMLM-pythia8', 'ZJetsToQQ_HT400to600_qc19_4j_TuneCP5_13TeV-madgraphMLM-pythia8', 'ZJetsToQQ_HT600to800_qc19_4j_TuneCP5_13TeV-madgraphMLM-pythia8', 'ggZH_HToTauTau_ZToLL_M125_13TeV_powheg_pythia8', 'ggZH_HToTauTau_ZToNuNu_M125_13TeV_powheg_pythia8', 'ggZH_HToTauTau_ZToQQ_M125_13TeV_powheg_pythia8', 'ttHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8'
-
 xs = {
     "WW_TuneCP5_13TeV-pythia8": 75.82,
     "WZ_TuneCP5_13TeV-pythia8": 27.6,
@@ -73,9 +71,22 @@ xs = {
     "ttHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8":0.5269*0.0627,
     "GluGluHToTauTau": 0.8045*0.0627, #from lhe (x BR)
     "GluGluHToWWToLNuQQ_M125_NNPDF31_TuneCP5_PSweights_13TeV_powheg_JHUGen710_pythia8":48.58*0.2137*2.*0.676*0.108,
-    "GluGluHToWWToLNuQQ_M125_TuneCP5_PSweight_13TeV-powheg2-jhugen727-pythia8":48.58*0.2137*2.*0.676*0.108
-    #"GluGluHToTauTau": 0.0398
+    "GluGluHToWWToLNuQQ_M125_TuneCP5_PSweight_13TeV-powheg2-jhugen727-pythia8":48.58*0.2137*2.*0.676*0.108,
+    "Spin0ToTauTau_2j_scalar_g1_HT300_M10_nodmx_v0_TuneCP5_MLM": 9.85e-02,
+    "Spin0ToTauTau_2j_scalar_g1_HT300_M30_nodmx_v0_TuneCP5_MLM": 1.78e-02,
+    "Spin0ToTauTau_2j_scalar_g1_HT300_M50_nodmx_v0_TuneCP5_MLM": 1.61e-02,
+    "Spin0ToTauTau_2j_scalar_g1_HT300_M125_nodmx_v0_TuneCP5_MLM": 1.28e-02,
+    "Spin0ToTauTau_2j_scalar_g1_HT300_M300_nodmx_v0_TuneCP5_MLM": 8.34e-03,
 }
+zkeys = []
+for d in xs.keys():
+    if "DYJetsToLL_Pt" in d:
+       zkeys.append(d)
+for z in zkeys:
+    xs[z+"_Zee"] = xs[z]
+    xs[z+"_Zmm"] = xs[z]
+    xs[z+"_Zem"] = xs[z]
+    xs[z+"_Ztt"] = xs[z]
 
 with open('xsec.json', 'w') as outfile:
     json.dump(xs, outfile, indent=4)
